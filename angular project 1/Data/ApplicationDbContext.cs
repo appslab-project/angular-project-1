e1b7b82930fca7aaa787079cc66727213ef4066e
Andrej Bakoš <77555201+cinusko@users.noreply.github.com>
2023-09-19 09:54:03 +0200

    Add project files.

---
+++ angular project 1/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using angular_project_1.Models;
+using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace angular_project_1.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
+            : base(options, operationalStoreOptions)
+        {
+
+        }
+    }
+}
