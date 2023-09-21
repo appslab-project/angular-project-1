6d32ac305fd33a2943a83bf7b00c24e7dde826a0
Pho3nixG0D <oliverkrska@gmail.com>
2023-09-21 08:16:06 +0200

    auto migration done, updated DB

--- angular project 1/Data/Migrations/20230921061449_DashboardMigration.Designer.cs
+++ angular project 1/Data/Migrations/20230921061449_DashboardMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using angular_project_1.Data;
 
@@ -11,9 +12,11 @@ using angular_project_1.Data;
 namespace angular_project_1.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230921061449_DashboardMigration")]
+    partial class DashboardMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- angular project 1/Models/ApplicationUser.cs
+++ angular project 1/Models/ApplicationUser.cs
@@ -4,6 +4,8 @@ namespace angular_project_1.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public int xp { get; set; }
+        public string guild { get; set; }
 
     }
 }
